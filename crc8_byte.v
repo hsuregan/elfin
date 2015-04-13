3cb45d8f44763340593faad6bc63dde89b718b1b
hsuregan <hsuregan@Gmail.com>
2015-04-12 20:34:13 -0700

    case logic

--- crc8_byte.v
+++ crc8_byte.v
@@ -10,7 +10,7 @@ module crc8_byte(
 	);
 
 
-	reg data[7:0]; //store 'in' into this reg 'data'
+	reg data[7:0]; //store 'in' into this shift reg 'data'
 	reg counter[2:0]; //at 3'b111; turn complete one
 	input cur_bit; //points to most significant bit in the input byte
 	assign cur_bit = data[7];
@@ -30,25 +30,34 @@ module crc8_byte(
 		data = in;
 		counter <= 0;
 		shift = 0;
-	end 
+	end
 
-	always @((posedge clk or negedge rst_n) and enable)
+	always @(negedge enable)
+	begin
+		shift <= 0;
+	end
+
+	always @(posedge enable)
+	begin
+		shift <= 1;
+	end
+
+	always @((posedge clk or negedge rst_n) and enable and (!clr))
 		begin
 			if((rst_n) && (counter < 0b'111)) //case in which counter is below 7
 			begin
-				counter <= counter + 1;
 				shift <= 1;
+				counter <= counter + 1;
 				data <= {data[6:0], reg[7]};
 			end else if(counter == 0b'111) //case in which a byte of data has been processed
 			begin
 				shift <= 0; //do not shift into pat's module
-				clr = 1; //clear data
 				counter <= 0; //set counter back to zero
 				complete = 1; //crc for byte complete
-			end else if(!rst_n) //case in which rst, set counter to 0, replace data with inp
+			end else if(!rst_n) //case in which rst, set counter to 0, INSTANTIATES 
+				data = in;
 				counter <= 0;
 				shift <= 0;
-				clr <= 1;
 			end
 		end
 
