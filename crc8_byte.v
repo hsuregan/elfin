f80a57b5f7c2897dabb4346d7a70e51d27931f91
hsuregan <hsuregan@Gmail.com>
2015-04-12 17:33:03 -0700

    initial counter added

--- crc8_byte.v
+++ crc8_byte.v
@@ -10,13 +10,15 @@ module crc8_byte(
 	);
 
 
-	reg data[7:0]; //store data into register
+	reg data[7:0]; //store data into shift register
+	reg counter[2:0]; //at 3'b111; turn complete one
 	input cur_bit; //points to most significant bit in the input byte
 	assign cur_bit = data[7];
 
-	always @(rst_n) //rst_n to initialize the temp data register (inital begin doesn't synthesize)
+	always @(rst_n) //rst_n to initialize the temp data reg; also resets temp register whenever rst_n is high
 		begin
 			data = in;
+			counter <= 0;
 		end
 
 	crc8 calculate(
@@ -30,9 +32,17 @@ module crc8_byte(
 
 	always @(posedge clk)
 		begin
-		if(enable)
-			shift <= 1;
-			data <= {data[6:0], reg[7]};
+			if(enable && (counter < 0b'111))
+			begin
+				counter <= counter + 1;
+				shift <= 1;
+				data <= {data[6:0], reg[7]};
+			end else if(counter == 0b'111)
+			begin
+				counter = 0b'0;
+			end else //enable is off
+			
+			end
 		end
 
 	
