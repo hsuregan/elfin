03e2a3f36700162db89b34de4e39d115b0b79433
hsuregan <hsuregan@Gmail.com>
2015-04-12 20:41:21 -0700

    modelsim project added

--- crc8.v
+++ crc8.v
@@ -15,7 +15,7 @@ module crc8(
    always @(posedge clk or negedge rst_n)
      if (!rst_n) crc <= 0;
      else if (clr) crc <= 0;
-     else if (shift) crc <= {crc_reg[6:3],crc_reg[7]^crc_reg[2],crc_reg[7]^crc_reg[1],crc_reg[7]^in};
+     else if (shift) crc <= {crc[6:3],crc[7]^crc[2],crc[7]^crc[1],crc[7]^in};
  
  
  
